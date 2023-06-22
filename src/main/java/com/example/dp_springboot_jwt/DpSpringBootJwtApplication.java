d2e70af91fa97243ea122682f76d1207d94e8362
Nadushanth Sathiskumar <snadushanth2000@gmail.com>
2023-06-22 14:29:24 +0530

    First Commit

---
+++ src/main/java/com/example/dp_springboot_jwt/DpSpringBootJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.dp_springboot_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpSpringBootJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpSpringBootJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dp_springboot_jwt/DpSpringBootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dp_springboot_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpSpringBootJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
